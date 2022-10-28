acc1ceb932145942772b8856a36f297c05682b04
Markus Doan-Le <markus.doanle@gmail.com>
2022-10-28 23:04:29 +0000

    New Example, Same Result

--- chart-js-test/src/index.jsx
+++ chart-js-test/src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+
+import ReactDOM from 'react-dom';
 import App from './linechart';
 
-const container = document.querySelector('#root');
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
--- chart-js-test/src/linechart.jsx
+++ chart-js-test/src/linechart.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
-
 import { Line } from 'react-chartjs-2';
 
-const data = {
-  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+const state = {
+  labels: ['January', 'February', 'March',
+    'April', 'May'],
   datasets: [
     {
-      label: 'First dataset',
-      data: [33, 53, 85, 41, 44, 65],
-      fill: true,
-      backgroundColor: 'rgba(75,192,192,0.2)',
-      borderColor: 'rgba(75,192,192,1)'
-    },
-    {
-      label: 'Second dataset',
-      data: [33, 25, 35, 51, 54, 76],
+      label: 'Rainfall',
       fill: false,
-      borderColor: '#742774'
+      lineTension: 0.5,
+      backgroundColor: 'rgba(75,192,192,1)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56]
     }
   ]
 };
 
-export default function App() {
-  return (
-    <div className="App">
-      <Line data={data} />
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Line
+          data={state}
+          options={{
+            title: {
+              display: true,
+              text: 'Average Rainfall per month',
+              fontSize: 20
+            },
+            legend: {
+              display: true,
+              position: 'right'
+            }
+          }}
+        />
+      </div>
+    );
+  }
 }
